283cea1b8044454f5ec5cd572f37962ad7f4d48f
Ssong10 <leesy1403@naver.com>
2019-09-26 14:26:35 +0900

    N과M3 수정

--- 지수쌤의 알고리즘강의/backtraking/BOK_15651_N과M3.py
+++ 지수쌤의 알고리즘강의/backtraking/BOK_15651_N과M3.py
@@ -1,11 +1,13 @@
-def back(choice):
-    if len(choice) == M:
-        print(*choice)
+def back(n):
+    if n == M:
+        print(*result)
     else:
-        for i in range(1,N+1):
-            choice.append(i)
-            back(choice)
-            choice.pop()
-        
-N,M = map(int,input().split())
-back([])
+        for i in range(1, N + 1):
+            result.append(i)
+            back(n + 1)
+            result.pop()
+
+
+N, M = map(int, input().split())
+result = []
+back(0)
