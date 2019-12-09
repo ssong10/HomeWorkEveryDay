159cf67568cec39ad79e713e67be510aadfb2974
ssong10 <leesy1403@naver.com>
2019-12-09 18:01:00 +0900

    17822원판돌리기

--- 17822_원판돌리기.py
+++ 17822_원판돌리기.py
@@ -4,31 +4,31 @@ pan = [deque(map(int,input().split())) for _ in range(N)]
 spins = [list(map(int,input().split())) for _ in range(T)]
 dy,dx = [-1,1,0,0] , [0,0,-1,1]
 for spin in spins:
-    [x,d,k] = spin
     for i in range(N):
-        if not (i+1) % x :
-            if d:
-                for _ in range(k):
+        if not (i+1) % spin[0] :
+            if spin[1]:
+                for _ in range(spin[2]):
                     pan[i].append(pan[i].popleft())
             else:
-                for _ in range(k):
+                for _ in range(spin[2]):
                     pan[i].appendleft(pan[i].pop())
     same = set()
     for y in range(N):
         for x in range(M):
-            a = pan[y][x]
-            if a:
+            if pan[y][x]:
                 for d in range(4):
                     yy,xx = y+dy[d],x+dx[d]
-                    if -1<yy<N and xx < M and a == pan[yy][xx]:
+                    if -1< yy< N and xx < M and pan[y][x] == pan[yy][xx]:
+                        if xx == -1:
+                            xx = M-1
                         same.add((y,x))
                         same.add((yy,xx))
     for yx in same:
-        pan[yx[0]][yx[1]] = False
+        pan[yx[0]][yx[1]] = 0
     result,cnt = 0,0
     for y in range(N):
         for x in range(M):
-            if pan[y][x] and (y,x) not in same:
+            if pan[y][x]:
                 result += pan[y][x]
                 cnt += 1
     if not same:
@@ -39,9 +39,7 @@ for spin in spins:
                 if pan[y][x]:
                     if pan[y][x] > avg:
                         pan[y][x] -= 1
-                    else:
+                    elif pan[y][x] < avg:
                         pan[y][x] += 1
                     result += pan[y][x]
-
 print(result)
-    
