cc0ee04d5dc96ee55c6a4ebaae4f5f1ed93ae819
ssong10 <leesy1403@naver.com>
2019-12-30 15:40:30 +0900

    BOJ_17779

--- BOJ/★Samsung/BOJ_17779_게리맨더링2.py
+++ BOJ/★Samsung/BOJ_17779_게리맨더링2.py
@@ -1,17 +1,27 @@
+def divide(y,x,d_1,d_2):
+    global result
+    tmp = [0,0,0,0,0]
+    for r in range(N):
+        for c in range(N):
+            if x+y <= r+c <= x+y+d_1+d_1 and x-y -d_2-d_2 <= c-r <= x-y:
+                tmp[4] += arr[r][c]
+            elif -1 < r < y+d_2 and -1 < c <= x:
+                tmp[0] += arr[r][c]
+            elif -1 < r <= y+d_1 and x < c <= N:
+                tmp[1] += arr[r][c]
+            elif y+d_2 <= r <= N and -1 < c < x-d_2+d_1:
+                tmp[2] += arr[r][c]
+            elif y+d_1 < r <= N and x- d_2 + d_1 <= c <= N:
+                tmp[3] += arr[r][c]
+    result = min(result,max(tmp)-min(tmp))
+
 N = int(input())
 arr = [list(map(int,input().split())) for _ in range(N)]
-print(arr)
-for x in range(N):
-    for y in range(N):
-        if x+y < 3 and y-x < 2:
-            arr[y][x] = 1
-        elif x+y < 3 and x- y < 2:
-            arr[y][x] = 2
-        elif x+y > 5 and y-x < 2:
-            arr[y][x] = 4
-        elif x+y > 5 and x-y < 2:
-            arr[y][x] = 3
-        else:
-            arr[y][x] = 5
-for a in arr:
-    print(a)
+result = 10 ** 6
+for a in range(2,N):
+    for b in range(1,a):
+        for y in range(N-a):
+            for x in range(a-b,N-b):
+                divide(y,x,b,a-b)
+
+print(result)
