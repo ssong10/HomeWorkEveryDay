1a2b146a05d18587969492b6b0e022fe65e052b6
ssong10 <leesy1403@naver.com>
2020-01-19 14:21:44 +0900

    boj_16985

--- BOJ/★Samsung/BOJ_16985_Maaaze.py
+++ BOJ/★Samsung/BOJ_16985_Maaaze.py
@@ -12,18 +12,35 @@ def go(tmp):
                         visit[zz][yy][xx] = 1
                         tmp.append((zz,yy,xx))
 
-def make(pan,n):
+def spin(pan,i):
+    if not i:
+        return pan
+    tmp = [[0]*5 for _ in range(5)]
+    for y in range(5):
+        for x in range(5):
+            tmp[x][4-y]= pan[y][x]
+    spin(tmp,i-1)
+
+def makeorder(panorder,n):
     if n ==5 :
-        print(pan)
+        make([],panorder,0)
         return
     for i in range(5):
         if not used[i]:
-            pan[n] = i
+            panorder[n] = i
             used[i] = 1
-            make(pan,n+1)
+            makeorder(panorder,n+1)
             used[i] =0
+
+def make(arr,panorder,i):
+    if i == 4:
+        print(arr)
+        return
+    for _ in range(3):
+        arr.append(spin(maze[panorder[i]])
 
 from collections import deque
 maze = [[list(map(int,input().split())) for _ in range(5)] for _ in range(5)]
-pan, used = [[] for _ in range(5)], [0] * 5
-make(pan,0)
+panorder, used = [[] for _ in range(5)], [0] * 5
+# makeorder(panorder,0)
+make([],[0,1,2,3,4],0)
