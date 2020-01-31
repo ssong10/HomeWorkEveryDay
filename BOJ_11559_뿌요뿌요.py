e2b71031502d214c72124a0b2760fa10513c7a0a
ssong10 <leesy1403@naver.com>
2020-01-31 17:34:18 +0900

    0131 | algorithms

--- BOJ_11559_뿌요뿌요.py
+++ BOJ_11559_뿌요뿌요.py
@@ -1,14 +1,23 @@
-def made(tmp):
-    global puyo
-    if tmp:
-        
-    else:
-        if len(tmp) > 3:
-            print(0)
+dy, dx = [-1,1,0,0],[0,0,-1,1]
+def made(y,x):
+    arr[y][x] = '.'
+    for d in range(4):
+        yy, xx = y+dy[d],x+dx[d]
+        if -1<yy<12 and -1 <xx < 6 and arr[yy][xx] == val:
+            made(yy,xx)            
+
+def bomb(arr):
+    bomblist = []
+    new_arr = [['.'] * 6 for _ in range(12)]
+    print(new_arr)
+    for y in range(12):
+        for x in range(6):
+            if arr[y][x] != '.':
+                val = arr[y][x]
+                made(y,x)
+    if bomblist:
+        bomb(new_arr)
 
 arr = [list(input()) for _ in range(12)]
-for y in range(12):
-    for x in range(6):
-        if arr[y][x] != '.':
-            puyo = [[y,x]]
-            made(puyo)
+result = 0
+bomb(arr)
