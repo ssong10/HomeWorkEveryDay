1fb0d385315c9eb5f1b2c5df364b162b9309f730
ssong10 <leesy1403@naver.com>
2020-02-21 23:37:04 +0900

    0221 | algo

--- SWEA_5656_벽돌깨기.py
+++ SWEA_5656_벽돌깨기.py
@@ -1,6 +1,3 @@
 for tc in range(int(input())):
     N, W, H = map(int,input().split())
     arr = [list(map(int,input().split())) for _ in range(H)]
-    for y in range(H):
-        for x in range(W):
-            
