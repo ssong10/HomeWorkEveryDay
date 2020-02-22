b304a536abe6f1bd98880174a31dc30f803bcab6
ssong10 <leesy1403@naver.com>
2020-02-22 21:28:08 +0900

    asdf

---
+++ BOJ_16987_겨란치기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def nextegg(eggs,hand,i,res):
+    global result
+    if eggs:
+        if eggs[i][0] < 0 :
+            res += 1
+            eggs.pop(i)
+        if hand[0] < 0:
+            hand = eggs.pop(0)
+            res += 1
+        print(hand,eggs,res)
+        for j in range(len(eggs)):
+            nextegg(eggs,hand,j,res)
+    else:
+        result = max(result,res)
+
+N = int(input())
+arr = [list(map(int,input().split())) for _ in range(N)]
+result = 0
+for i in range(N-1):
+    nextegg(arr[1:],arr[0],i,0)
+print(result)
