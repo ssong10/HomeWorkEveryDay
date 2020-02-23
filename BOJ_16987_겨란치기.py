c5323b45dc9ee637ebb2570f66de79ed08ba0361
ssong10 <leesy1403@naver.com>
2020-02-23 23:42:04 +0900

    0223 SY algorithms

--- BOJ_16987_겨란치기.py
+++ BOJ_16987_겨란치기.py
@@ -3,15 +3,17 @@ from collections import deque
 def nextegg(eggs,hand,i,res):
     global result
     if eggs:
+        eggs[i][0] -= hand[1]
+        hand[0] -= eggs[i][1]
         if eggs[i][0] < 0 :
             res += 1
             eggs.pop(i)
         if hand[0] < 0:
             hand = eggs.pop(0)
             res += 1
-        print(hand,eggs,res)
         for j in range(len(eggs)):
             nextegg(eggs,hand,j,res)
+            
     else:
         result = max(result,res)
 
--- SWEA_5656_벽돌깨기.py
+++ SWEA_5656_벽돌깨기.py
@@ -1,3 +1,23 @@
+def drop(i):
+    for y in range(H):
+        if arr[y][i]:
+            n = arr[y][i]
+            bomb = [y,i,n]
+            break
+    bomblist.append(bomb)
+    for y in range(bomb[0]-n+1,bomb[0]+n-1):
+        if -1 < y < H:
+            if y != bomb[0] and arr[y][i]:
+                bomblist.append([y,i,arr[y][i]])
+    for x in range(i-n+1,i+n-1):
+        if -1 < x < W:
+            if x != i and arr[bomb[0]][x]:
+                bomblist.append([bomb[0],x,arr[bomb[0]][x]])
+    print(bomblist)
+
 for tc in range(int(input())):
-    N, W, H = map(int,input().split())
+    N, W, H = map(int,input().split()) #  W, H | 가로, 세로
     arr = [list(map(int,input().split())) for _ in range(H)]
+    for i in range(W):
+        bomblist = []
+        drop(i)
