218ad8486599c7d483afd942aab9797ce8529fd6
ssong10 <leesy1403@naver.com>
2020-04-15 00:00:06 +0900

    asdf

--- SWEA_보급로.py
+++ SWEA_보급로.py
@@ -2,15 +2,14 @@ dy,dx = [1,0,-1,0],[0,1,0,-1]
 
 def bfs(tmp):
     global result
-    tmp2 = []
-    for [y,x] in tmp:
+    while tmp:
+        y,x = tmp.popleft()
         for d in range(4):
             yy,xx = y+dy[d],x+dx[d]
             if -1 < yy < N and -1 < xx < N:
-                next,val = visit[yy][xx], visit[y][x] + arr[yy][xx]
-                if not next or (next and val < next):
+                if not visit[yy][xx] or (visit[yy][xx] and visit[yy][xx] > visit[y][x]+arr[yy][xx]):
                     visit[yy][xx] = val
-                    tmp2.append([yy,xx])
+                    tmp.append([yy,xx,dd])
     if tmp2:
         bfs(tmp2)
 for tc in range(int(input())):
---
+++ SWEA_보급로_2.py
@@ -0,0 +1,5 @@
+for tc in range(int(input())):
+    N = int(input())
+    arr = [list(map(int,input())) for _ in range(N)]
+    stack = []
+    
