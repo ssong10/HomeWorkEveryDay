15e783e99f7a702cc8a3500936ea66fb8a9d8099
ssong10 <leesy1403@naver.com>
2021-02-02 23:42:28 +0900

    algo

--- BOJ/BOJ_2156_포도주시식.py
+++ BOJ/BOJ_2156_포도주시식.py
@@ -1,7 +1,14 @@
-N = int(input())
-step = [0,0,0] + [int(input()) for _ in range(N)]
-
-dp = [0] * (N+3)
-for i in range(N):
-  dp[i+3] = max(dp[i]+step[i+2]+step[i+3], dp[i+1]+step[i+3],dp[i+2])
-print(dp[-1])
+import sys
+input = sys.stdin.readline
+n = int(input())
+step = []
+for _ in range(n):
+    step.append(int(input()))
+dp = [step[0]]
+for i in range(1,n):
+    one = dp[i-2]+step[i]
+    two = step[i-1]+step[i]
+    if i-3>-1:
+        two+=dp[i-3]
+    dp.append(max(one,two))
+print(dp)
