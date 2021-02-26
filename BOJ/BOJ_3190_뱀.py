ba1272a45d2443fa5511f58103e83fa6a48804bf
ssong10 <leesy1403@naver.com>
2021-02-27 01:40:55 +0900

    algo

--- BOJ/BOJ_3190_뱀.py
+++ BOJ/BOJ_3190_뱀.py
@@ -15,26 +15,36 @@ for _ in range(K):
 time = 0
 L = int(input())
 moves = [list(input().split()) for _ in range(L)]
-
+move_cnt = 0
 while True:
   time += 1
-  for b in board:
-    print(*b)
-  print('-----')
-  print(head,tail)
   hy,hx = head
   nhy,nhx = hy+dy[d], hx+dx[d]
   if -1 < nhy < N and -1< nhx < N:
-    # 사과
     if board[nhy][nhx] == 4:
       pass
     elif board[nhy][nhx] == -1:
+      # 빈칸일때만 꼬리 바꿔주기
       ty,tx = tail
       td = board[ty][tx]
       tail = (ty+dy[td],tx+dx[td])
       board[ty][tx] = -1
     else:
       break
+
+    # 움직이는지
+    if move_cnt < len(moves) and int(moves[move_cnt][0]) == time:
+      if moves[move_cnt][1] == 'D':
+        d = (d+1) % 4
+      else:
+        d = (d-1) % 4
+      move_cnt += 1
+
+    # 머리 바꿔주기
+    head = (nhy,nhx)
+
+    # 머리방향
+    board[nhy][nhx] = d
   else:
     break
 print(time)
