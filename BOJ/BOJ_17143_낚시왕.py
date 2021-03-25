f9f1be7e958d4e4a74e9c8b5f002b3642a48d134
ssong10 <leesy1403@naver.com>
2021-03-25 22:45:42 +0900

    낚시왕

--- BOJ/BOJ_17143_낚시왕.py
+++ BOJ/BOJ_17143_낚시왕.py
@@ -1,30 +1,39 @@
 R, C, M = map(int,input().split())
 dy,dx = [-1,1,0,0],[0,0,1,-1]
+next_d = [1,0,3,2]
 sharks = [[False for _ in range(C)] for _ in range(R)]
 for _ in range(M):
     r,c,s,d,z = map(int,input().split())
     sharks[r-1][c-1] = (s,d-1,z)
-
-t=0
-for i in range(R):
-    if sharks[i][t]:
-        s,d,z = sharks[i][t]
-        print(z)
-        sharks[i][t] = False
-        break
-move = {}
-for y in range(R):
-    for x in range(C):
-        if sharks[y][x]:
-            s,d,z=sharks[y][x]
-            yy,xx = (y + (dy[d] * s)) % (2*R-2), (x+(dx[d]*s)) % (2*C-2)
-            dd = d
-            if yy > R:
-                dd += d%2
-                yy = 2*R-2-yy
-            if xx > R:
-                dd += d%2
-                xx = 2*C-2-xx
-
-            print(yy,xx,dd)
-print(sharks)
+answer = 0
+for t in range(C):
+    for i in range(R):
+        if sharks[i][t]:
+            s,d,z = sharks[i][t]
+            answer += z
+            sharks[i][t] = False
+            break
+    move = {}
+    for y in range(R):
+        for x in range(C):
+            if sharks[y][x]:
+                s,d,z=sharks[y][x]
+                yy,xx = (y + (dy[d] * s)) % (2*R-2), (x+(dx[d]*s)) % (2*C-2)
+                dd = d
+                if yy >= R:
+                    dd = next_d[d]
+                    yy = 2*R-2-yy
+                if xx >= C:
+                    dd = next_d[d]
+                    xx = 2*C-2-xx
+                if (yy,xx) in move:
+                    (ss,sd,sz)= move[(yy,xx)]
+                    if sz < z:
+                        move[(yy,xx)] = (s,dd,z)
+                else:
+                    move[(yy,xx)] = (s,dd,z)
+                sharks[y][x] = False
+    for (y,x) in move:
+        s,d,z = move[(y,x)]
+        sharks[y][x] = (s,d,z)
+print(answer)
