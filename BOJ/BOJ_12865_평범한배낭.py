f3c21a0ef6bfdf7acd96f84fc6939e7919ce51f7
ssong10 <leesy1403@naver.com>
2021-04-03 23:36:59 +0900

    algo

--- BOJ/BOJ_12865_평범한배낭.py
+++ BOJ/BOJ_12865_평범한배낭.py
@@ -1,9 +1,14 @@
 N, K = map(int, input().split())
-items = [list(map(int,input().split())) for _ in range(N)]
-weight = [0] * K
+items = []
 for i in range(N):
-  w, v = items[i]
-  v += weight[w-1]
-  for j in range(i,n):
-    if (w+items[])
-  print(weight)
+  w,v = map(int,input().split())
+  items.append([w,v])
+weight = [[0] * (K+1) for _ in range(N+1)]
+for i in range(1,N+1):
+  for j in range(1,K+1):
+    w,v = items[i-1]
+    if w <= j:
+      weight[i][j] = max(v + weight[i-1][j-w],weight[i-1][j])
+    else:
+      weight[i][j] = weight[i-1][j]
+print(weight[N][K])
