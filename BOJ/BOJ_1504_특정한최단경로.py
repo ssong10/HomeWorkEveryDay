c3a59bb5f742d24f8664692b98c76b4374522291
ssong10 <leesy1403@naver.com>
2021-05-04 23:59:40 +0900

    특정한최단경로;

--- BOJ/BOJ_1504_특정한최단경로.py
+++ BOJ/BOJ_1504_특정한최단경로.py
@@ -1,34 +1,43 @@
 import heapq
 import sys
 input = sys.stdin.readline
-
-def find(start,end):
+MAX = 10 ** 6
+def find(start):
+  answer = [MAX] * N
   visit = [False] * N
   visit[start] = True
   tmp = []
   for t in tree[start]:
     heapq.heappush(tmp,t)
-  while heapq:
+  while sum(visit) < N:
     t = heapq.heappop(tmp)
     v,n = t
-    if n == end:
-      return
     if not visit[n]:
       visit[n] = True
-      answer[n] =  min(answer[n], n)
+      answer[n] =  min(answer[n], v)
       for vv, nn in tree[n]:
-        if not visit[nn] and answer[nn] > vv:
-          answer[nn] = vv
-          heapq.heappush(tmp,(vv,nn))
-
+        if not visit[nn] and answer[nn] > v+vv:
+          answer[nn] = v+vv
+          heapq.heappush(tmp,(v+vv,nn))
+  print(answer)
+  return answer
 N, E = map(int,input().split())
 tree = [[] for _ in range(N+1)]
-answer = [10**6] * N
 for _ in range(E):
   a,b,c = map(int,input().split())
   tree[a-1].append((c,b-1))
   tree[b-1].append((c,a-1))
 v1,v2 = map(int,input().split())
-print(tree)
-find(0,v1)
-find(0,v2)
+for (n,v) in tree[v1-1]:
+  if v2 == v:
+    result = n
+graph1 = find(0)
+graph2 = find(N-1)
+
+tmp = MAX
+if graph1[v1-1] != MAX and graph2[v2-1] != MAX:
+  tmp = min(tmp,graph1[v1-1] + graph2[v2-1])
+if graph2[v1-1] != MAX and graph1[v2-1] != MAX:
+  tmp = min(tmp,graph2[v1-1] + graph1[v2-1])
+print(tmp,result)
+print(tmp+result)
