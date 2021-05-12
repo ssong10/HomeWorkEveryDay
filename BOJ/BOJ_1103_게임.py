646ada597d3674f71f70008c3a3642ee3ac95f55
ssong10 <leesy1403@naver.com>
2021-05-13 00:00:20 +0900

    게임

--- BOJ/BOJ_1103_게임.py
+++ BOJ/BOJ_1103_게임.py
@@ -1,27 +1,37 @@
-from collections import deque
-dy,dx = [-1,0,1,0],[0,1,0,-1]
+import sys
+sys.setrecursionlimit(10**8)
 
+dy,dx = [-1,0,1,0],[0,1,0,-1]
+def go(y,x):
+  global flag
+  if answer[y][x]:
+    return answer[y][x]
+  if not flag:
+    return 0
+  X = board[y][x]
+  for d in range(4):
+    yy,xx = y+dy[d]*X , x+dx[d]*X
+    if -1<yy<N and -1<xx<M and board[yy][xx] != 'H':
+      if visit[yy][xx]:
+        flag = False
+      visit[yy][xx] = True
+      tmp = go(yy,xx)
+      if answer[y][x] <= tmp:
+        answer[y][x] = tmp + 1
+      visit[yy][xx] = False
+  return answer[y][x]
+        
 N, M = map(int,input().split())
 board = [list(input()) for _ in range(N)]
 for y in range(N):
   for x in range(M):
     if board[y][x] != 'H':
       board[y][x] = int(board[y][x])
-result = 0
-visit = [[0] * M for _ in range(N)]
-
-tmp = deque([[0,0]])
-for d in range(4):
-  yy,xx = y+dy[d]*X , x+dx[d]*X
-  if -1<yy<N and -1<xx<M and board[yy][xx] != 'H':
-    if visit[yy][xx]:
-      result = -1
-      return
-    else:
-      go(yy,xx,count+1)
-  else:
-    if result != -1:
-      result = max(result,count+1)
-
-
-print(result)
+answer = [[0] * M for _ in range(N)]
+visit = [[False] * M for _ in range(N)]
+flag = True
+go(0,0)
+if flag:
+  print(answer[0][0]+1)
+else:
+  print(-1)
